764e4b86872a0940e4ad1b8f2cb2f291a0d88bd8
Rajan Karki <rajankarkee8@gmail.com>
2025-01-17 01:25:24 +0545

    update: models and urls for post functionality

--- post/models.py
+++ post/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length= 255,blank=True, null=True)
+    author = models.ForeignKey(User, on_delete= models.CASCADE)
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    desc = models.TextField()
+    comment_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'{self.post}, {self.author}'
+    
--- post/views.py
+++ post/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Post, Comment
+from .forms import CommentForm
 
-# Create your views here.
+def Home(request):
+    posts = Post.objects.all().order_by('-date_created')
+    context = {
+        'posts':posts
+    }
+    return render(request, 'post/listing.html', context)
+
+def Post_details(request, pk):
+    post = Post.objects.get(id=pk)
+    context = {
+        'post':post
+    }
+    return render(request, 'post/post_detail.html', context)
+
+def PostComment(request, pk):
+    post = get_object_or_404(Post, id=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comments = form.save(commit=False)
+            comments.post = post
+            comments.user = request.user
+            comments.save()
+            return redirect('details', pk=post.pk) 
+    else:
+        form = CommentForm()
+        context = {'form': form, 'post': post}
+    return render(request, 'post/post_comment.html',context)
